b29f31b70a835728d65dd57d1503af5a28d222b0
Marcx <haitianhero6@gmail.com>
2024-08-15 16:07:23 -0400

    created ERC token along with MerkleAirdrop contract

--- src/MerkleAirdrop.sol
+++ src/MerkleAirdrop.sol
@@ -1,7 +1,12 @@
 //SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.24;
 
 contract MerkleAirdrop {
+    //some list of addresses
+    //allow someone in the list to claim a tokens
+    address[] public claimers;
+
+    //going to use merkle proofs
     
 }

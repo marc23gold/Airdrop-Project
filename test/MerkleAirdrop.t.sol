69a3b320d0184941c9e2840550736fa104073a0a
Marcx <haitianhero6@gmail.com>
2024-08-16 14:20:24 -0400

    created script for merkle root proofs to generate

---
+++ test/MerkleAirdrop.t.sol
@@ -0,0 +1,15 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+import {Test, console} from "forge-std/Test.sol";
+
+contract MerkleAirdrop is Test{
+
+    //init variables for contracts passed in and pranks
+    
+    function setUp() public {
+        //deploy the contract
+        //pass in the merkle root and the airdrop token 
+    }
+}

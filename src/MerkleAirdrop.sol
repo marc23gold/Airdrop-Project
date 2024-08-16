ea60eca510cebcc4c0d99c5ad44706301097f249
Marcx <haitianhero6@gmail.com>
2024-08-16 11:48:27 -0400

    inital merkleairdrop constructor

--- src/MerkleAirdrop.sol
+++ src/MerkleAirdrop.sol
@@ -2,11 +2,18 @@
 
 pragma solidity ^0.8.24;
 
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 contract MerkleAirdrop {
     //some list of addresses
     //allow someone in the list to claim a tokens
     address[] public claimers;
+    bytes32 private immutable i_merkleRoot;
+    IERC20 private immutable i_airdropToken;
 
     //going to use merkle proofs
-    
+    constructor(bytes32 _merkleRoot, IERC20 _airdropToken) {
+        i_merkleRoot = _merkleRoot;
+        i_airdropToken = _airdropToken;
+    }
 }

b5d025ca6f6d743829320e053c322edb4cd7a139
Marcx <haitianhero6@gmail.com>
2024-08-16 15:39:48 -0400

    refactored tests and imports

--- test/MerkleAirdropTest.t.sol
+++ test/MerkleAirdropTest.t.sol
@@ -3,13 +3,23 @@
 pragma solidity ^0.8.24;
 
 import {Test, console} from "forge-std/Test.sol";
+import {MerkleAirdrop} from "../src/MerkleAirdrop.sol";
+import {DogToken} from "../src/DogToken.sol";
 
-contract MerkleAirdrop is Test{
+contract MerkleAirdropTest is Test{
 
     //init variables for contracts passed in and pranks
-    
+
     function setUp() public {
         //deploy the contract
         //pass in the merkle root and the airdrop token 
+    }
+
+    function testUsersCanClaim () public {
+        //arrange
+
+        //act
+
+        //assert
     }
 }

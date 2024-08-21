f20f6d4bb64b175a90842127373e5e3de72ae1ce
Marcx <haitianhero6@gmail.com>
2024-08-20 20:33:39 -0400

    interactions script

---
+++ script/Interact.s.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+import {Script, console} from "forge-std/Script.sol";
+import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
+
+contract Interact is Script {
+    
+}
